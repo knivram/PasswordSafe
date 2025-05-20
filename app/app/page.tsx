bc2e7c5328a8482557aa9d0a542cc3d8a19f27ce
knivram <129607867+knivram@users.noreply.github.com>
2025-05-20 15:08:24 +0200

    implemented private key decryption and keyStore

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { useKeyStore } from "../context/KeyStore";
+
 export default function Page() {
+  const { isInitialized, privateKey, publicKey } = useKeyStore();
+
+  if (!isInitialized) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
-      <h1>Page</h1>
+      <p>Private Key: {JSON.stringify(privateKey.algorithm)}</p>
+      <p>Public Key: {JSON.stringify(publicKey.algorithm)}</p>
     </div>
   );
 }
---
+++ app/context/KeyStore.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { getUserData } from "../actions/getUserData";
+import { CryptoService } from "@/lib/crypto";
+
+const cryptoService = new CryptoService();
+
+interface KeyStoreInitContextType {
+  isInitialized: boolean;
+  isDialogOpen: boolean;
+  initializeKeyStore: (masterPassword: string) => Promise<void>;
+  clearKeyStore: () => void;
+  openDialog: () => void;
+  closeDialog: () => void;
+}
+
+type KeyStoreReadContextType =
+  | {
+      isInitialized: false;
+      publicKey: null;
+      privateKey: null;
+    }
+  | {
+      isInitialized: true;
+      publicKey: CryptoKey;
+      privateKey: CryptoKey;
+    };
+
+const KeyStoreInitContext = createContext<KeyStoreInitContextType>(null!);
+const KeyStoreReadContext = createContext<KeyStoreReadContextType>({
+  isInitialized: false,
+  publicKey: null,
+  privateKey: null,
+});
+
+export function KeyStoreProvider({ children }: { children: ReactNode }) {
+  const [privateKey, setPrivateKey] = useState<CryptoKey | null>(null);
+  const [publicKey, setPublicKey] = useState<CryptoKey | null>(null);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const isInitialized = privateKey !== null && publicKey !== null;
+
+  useEffect(() => {
+    if (!isInitialized && !isDialogOpen) {
+      setIsDialogOpen(true);
+    }
+  }, [isInitialized, isDialogOpen]);
+
+  const initializeKeyStore = useCallback(async (masterPassword: string) => {
+    try {
+      const userData = await getUserData();
+
+      const { publicKey, privateKey } =
+        await cryptoService.importPublicPrivateKey({
+          publicKeyBase64: userData.publicKey,
+          wrappedPrivateKeyBase64: userData.wrappedPrivateKey,
+          password: masterPassword,
+          salt: userData.salt,
+        });
+      setPrivateKey(privateKey);
+      setPublicKey(publicKey);
+
+      setIsDialogOpen(false);
+    } catch (error) {
+      console.error("Failed to initialize key store:", error);
+      throw error;
+    }
+  }, []);
+
+  const clearKeyStore = useCallback(() => {
+    setPrivateKey(null);
+    setPublicKey(null);
+  }, []);
+
+  const openDialog = useCallback(() => {
+    setIsDialogOpen(true);
+  }, []);
+
+  const closeDialog = useCallback(() => {
+    if (isInitialized) {
+      setIsDialogOpen(false);
+    }
+  }, [isInitialized]);
+
+  return (
+    <KeyStoreInitContext.Provider
+      value={{
+        isInitialized,
+        isDialogOpen,
+        initializeKeyStore,
+        clearKeyStore,
+        openDialog,
+        closeDialog,
+      }}
+    >
+      <KeyStoreReadContext.Provider
+        value={
+          isInitialized
+            ? {
+                isInitialized: true,
+                privateKey,
+                publicKey,
+              }
+            : {
+                isInitialized: false,
+                privateKey: null,
+                publicKey: null,
+              }
+        }
+      >
+        {children}
+      </KeyStoreReadContext.Provider>
+    </KeyStoreInitContext.Provider>
+  );
+}
+
+export const useKeyStoreInit = () => useContext(KeyStoreInitContext);
+
+export const useKeyStore = () => useContext(KeyStoreReadContext);
